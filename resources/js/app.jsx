7093abdbd566f6355ab62c765a1abefcd9c7ede5
cypher2014 <mditta3@estudiantes.areandina.edu.co>
2025-08-31 21:10:52 -0500

    Conexion UI CON API

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,10 +1,9 @@
 import '../css/app.css';
-
+import './bootstrap';
 
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
-import {DangerButton} from  './components/DangerButton'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -18,9 +17,7 @@ createInertiaApp({
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<><App {...props} />
-        <DangerButton/>
-        </>);
+        root.render(<App {...props} />);
     },
     progress: {
         color: '#4B5563',
